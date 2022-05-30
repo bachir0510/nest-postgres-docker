5828785cf736f4f6001489f2bca3c90f0aaf2044
Bachir05 <bachirsalek0598@gmail.com>
2022-05-30 09:16:10 +0200

    config jest typeorm

--- test/student.controller.spec.ts
+++ test/student.controller.spec.ts
@@ -1,21 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentController } from '../src/app/controllers/v1/student/student.controller';
 import { CreatStudent, GetSutdents } from '../src/domain/use_cases/student';
-import { connectionTest } from './connectionTest';
+import {TestHelper } from './testHeader';
+
+
+beforeAll(async () => {
+  await TestHelper.instance.setupTestDB();
+});
+
+afterAll(() => {
+  TestHelper.instance.teardownTestDB();
+});
 
 describe('StudentController', () => {
   let controller: StudentController;
 
-  beforeAll(async () => {
-    await connectionTest.create();
-  });
-
-  afterAll(async () => {
-    await connectionTest.close();
-  });
 
   beforeEach(async () => {
-    await connectionTest.clear();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentController],
       providers: [CreatStudent, GetSutdents],
