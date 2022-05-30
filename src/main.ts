798ed99da44c6dc4a9c459016bf0bef3875ac8cb
MARIA JOSE SANCHEZ DORIA <maria.sanchez@the-cocktail.com>
2022-05-31 00:29:47 +0200

    feat: config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as dotenv from "dotenv"
+import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
+
+const URL_SWAGGER = 'api/v1/docs/';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-//   dotenv.config()
-//   const config = new DocumentBuilder()
-//   .setTitle('Cats example')
-//   .setDescription('The cats API description')
-//   .setVersion('1.0')
-//   .addTag('cats')
-//   .build();
-// const document = SwaggerModule.createDocument(app, config);
-// SwaggerModule.setup('api', app, document);
+  dotenv.config();
+  const options = new DocumentBuilder()
+    .setTitle('Students API')
+    .setDescription('The Students API description')
+    .setVersion('1.0')
+    .build();
+  const documentV1 = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(URL_SWAGGER, app, documentV1);
+  Logger.log('Swagger configured');
 
-
-await app.listen(AppModule.port);
+  await app.listen(AppModule.port);
+  Logger.log(`Server start on port: ${AppModule.port}`);
 }
 bootstrap();
