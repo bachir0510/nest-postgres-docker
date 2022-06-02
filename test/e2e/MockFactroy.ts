650a2e93761f6258fe6a3f613048b8fe9a160185
Bachir05 <bachirsalek0598@gmail.com>
2022-06-02 17:22:59 +0200

    testing student e2e

---
+++ test/e2e/MockFactroy.ts
@@ -0,0 +1,8 @@
+import { CreateStudentDTO } from "../../src/domain/dto/student/createStudent.dto";
+
+export class MockFactory {
+	genCreateNoteDto() {
+		const dto = new CreateStudentDTO();
+		return dto;
+	}
+}
--- test/e2e/student.e2e.spec.ts
+++ test/e2e/student.e2e.spec.ts
@@ -2,10 +2,13 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { Connection } from 'typeorm';
 import { testsAppModule } from '../test.app.module.factory';
+import { MockFactory } from './MockFactroy';
 
 describe('StudentController (e2e)', () => {
   let appTest: INestApplication;
   let database: Connection;
+  const mockFactory = new MockFactory();
+  const newStudent = mockFactory.genCreateNoteDto();
 
   beforeEach(async () => {
     const [app, nestModule] = await testsAppModule();
@@ -19,5 +22,18 @@ describe('StudentController (e2e)', () => {
 
   it('/student (GET)', () => {
     return request(appTest.getHttpServer()).get('/student').expect(200);
+  });
+
+  it('/student (POST)', async () => {
+    try {
+      const response = await request(appTest.getHttpServer())
+        .post('/student')
+        .send(newStudent);
+        
+      expect(response.status).toBe(201);
+      expect(typeof response.body).toBe('object');
+    } catch (err) {
+      throw err;
+    }
   });
 });
