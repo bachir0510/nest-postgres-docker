7d56ce04737c5797b75ed8114abeed553b8d6a43
MARIA JOSE SANCHEZ DORIA <maria.sanchez@the-cocktail.com>
2022-06-03 08:59:10 +0200

    feat: add test for student controller - get all

--- test/use-cases/student/getAll.student.spec.ts
+++ test/use-cases/student/getAll.student.spec.ts
@@ -1,33 +1,29 @@
 import { Test } from '@nestjs/testing';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { CreateStudentDTO } from '../../../src/domain/dto/student/createStudent.dto';
+import { Student } from '../../../src/domain/entitys/student.entity';
 import { GetStudents } from '../../../src/domain/use_cases/student';
 import { testsAppModule } from '../../test.app.module.factory';
-
-const studentData = {
-  nia: '151515',
-  name: 'Alberto',
-  lastName: 'PapaAlberto',
-  motherName: 'MamaAlberto',
-  group: '1',
-  classGroup: 'a',
-};
 
 describe('StudentController', () => {
   let database: Connection;
   let getStudent: GetStudents;
+  let studentEntity: Repository<Student>;
 
   beforeAll(async () => {
     const [nestModule] = await testsAppModule();
     getStudent = nestModule.get(GetStudents);
+    studentEntity = nestModule.get(Student.name);
     database = nestModule.get('DATABASE_CONNECTION');
+
   });
 
-  afterAll(() => {
+  afterAll(async () => {
+    await database.dropDatabase();
     database.close();
   });
 
-  beforeEach(async () => {
+  beforeEach(() => {
     Test.createTestingModule({
       providers: [GetStudents],
     });
@@ -37,8 +33,27 @@ describe('StudentController', () => {
     expect(getStudent).toBeDefined();
   });
 
-  it('Get all Students', async () => {
-    it('should find all solicitudes', async () => { 
+  describe('Get all Students', () => {
+    const studentData: Student = {
+      nia: '151515',
+      name: 'Alberto',
+      lastName: 'PapaAlberto',
+      motherName: 'MamaAlberto',
+      group: '1',
+      classGroup: 'a',
+      id: expect.any(Number),
+    };
+    it('should find all solicitudes', async () => {
+      const entity = {
+        nia: '151515',
+        name: 'Alberto',
+        lastName: 'PapaAlberto',
+        motherName: 'MamaAlberto',
+        group: '1',
+        classGroup: 'a',
+      };
+      await studentEntity.save(entity);
+
       expect(await getStudent.call()).toEqual([studentData]);
     });
   });
