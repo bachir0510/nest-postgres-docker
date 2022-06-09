214cc8a790c060f3d2ce18d20567f69d9a704e48
Bachir05 <bachirsalek0598@gmail.com>
2022-06-09 10:21:33 +0200

    feat: create jwt strategy

--- src/domain/use_cases/auth/auth.module.ts
+++ src/domain/use_cases/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from '../../strategy/jwt.strategy';
 import { UserModule } from '../user/user.module';
 import { LoginUser, RegisterUser } from './';
 
 @Module({
-  imports: [PassportModule, UserModule],
-  providers: [LoginUser, RegisterUser],
-  exports: [LoginUser, RegisterUser],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    UserModule,
+  ],
+  providers: [LoginUser, RegisterUser, JwtStrategy],
+  exports: [LoginUser, RegisterUser, JwtStrategy, PassportModule],
 })
 export class AuthModule {}
