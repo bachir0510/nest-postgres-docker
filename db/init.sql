73a8b6731b9a8ebd765292de67500ce5ac15127d
MARIA JOSE SANCHEZ DORIA <maria.sanchez@the-cocktail.com>
2022-06-22 07:58:49 +0200

    fix: resolving create database app with docker

---
+++ db/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE school_app;
+GRANT ALL PRIVILEGES ON DATABASE school_app TO postgres;
--- src/infrastructure/database/database.providers.ts
+++ src/infrastructure/database/database.providers.ts
@@ -1,21 +1,27 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 import * as path from 'path';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => {
-      const fn = await createConnection({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
-        entities: [path.resolve(__dirname + '/../../**/*.entity{.ts,.js}')],
-        synchronize: true,
-      });
-      return fn;
+      let connection: Connection;
+      try {
+        connection = await createConnection({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'postgres',
+          database: 'school_app',
+          entities: [path.resolve(__dirname + '/../../**/*.entity{.ts,.js}')],
+          synchronize: true,
+          dropSchema: true,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+      return connection;
     },
   },
 ];
