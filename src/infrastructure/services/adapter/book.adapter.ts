05d5195d6133a4916e872b800bd0acf87484d3fa
Bachir05 <bachirsalek0598@gmail.com>
2022-06-30 10:58:32 +0200

    feat: book adapter

--- src/infrastructure/services/adapter/book.adapter.ts
+++ src/infrastructure/services/adapter/book.adapter.ts
@@ -4,23 +4,5 @@ import { IBook } from '../interface/book.interface';
 @Injectable()
 export class BookAdapter {
   static maperUserResponse(data) {
-    const urlData: IBook = [
-      {
-        id: data.id,
-        title: data.title,
-        author: data.author,
-        categories: data.categories,
-      },
-    ];
-
-    const response = urlData.map((itm) => {
-      return {
-        id: data.id,
-        title: data.title,
-        author: data.author,
-        categories: data.categories,
-      };
-    });
-    return response;
-  }
-}
+    
+}}
